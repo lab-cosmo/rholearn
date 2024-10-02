5497a013f1b99164ed248ac5a5de23f94618128b
Joseph W. Abbott <joseph.william.abbott@gmail.com>
2024-10-02 17:26:07 +0200

    Update tutorial paths

--- example/rholearn-aims-tutorial/part-1-dft/dft_settings.py
+++ example/rholearn-aims-tutorial/part-1-dft/dft_settings.py
@@ -8,12 +8,14 @@ defaults - thus only overwriting the keys that are present in the user settings.
 Any variables defined here must be included in the final dictionary `DFT_SETTINGS` at
 the end of this file.
 """
+from os.path import join
 
 # Absolute path to the top directory where FHI-aims output data will be stored
-DATA_DIR = "path/to/example/rholearn-aims-tutorial/part-1-dft/data"
+TUTORIAL_DIR = "/path/to/rholearn/example/rholearn-aims-tutorial"
+DATA_DIR = join(TUTORIAL_DIR, "part-1-dft/data/")
 
 # Absolute path to .xyz file containing dataset
-XYZ = "path/to/frames.xyz"
+XYZ = join(DATA_DIR, "qm7.xyz")
 
 # List of frame indices for the above dataset to compute reference data for
 FRAME_IDXS = list(range(32))
@@ -22,7 +24,7 @@ FRAME_IDXS = list(range(32))
 AIMS_COMMAND = "srun /path/to/aims.x < control.in > aims.out"
 
 # Absolute path to species defaults directory
-SPECIES_DEFAULTS = "/path/to/species_defaults/light/default"
+SPECIES_DEFAULTS = join(TUTORIAL_DIR, "part-1-dft/species_defaults/light/default")
 
 # Other user settings here:
 # ...
